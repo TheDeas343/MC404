1e9049d4413431c80ab8a553e111b117e3c7d207
TheDeas343 <deascisi@hotmail.com>
2022-09-30 17:20:00 -0300

    Lab05

--- Labs/lab05/raiz.s
+++ Labs/lab05/raiz.s
@@ -1,17 +1,13 @@
-.set num_it, 10
-
-.data
-    count: .word 1
 .bss
     input: .skip 20         # buffer
-    num .skip 4
+    output: .skip 20
 
 .text
 
 write:
     li a0, 1                # file descriptor = 1 (stdout)
-    la a1, input            # buffer
-    li a2, 20               # size
+    la a1, output            # buffer
+    li a2, 20             # size
     li a7, 64               # syscall write (64)
     ecall
     ret
@@ -27,51 +23,116 @@ read:
 Str_Int:
     li a7, 10
     li a3, 0
-    la a0, input
-    la a2, count           
-
-    lb a1, count(a0)        # Pegando o primeiro byte da string
-    addi a1, a1, -'0'       # Transferindo para numero
+             
+    lb a1, 3(a0)        # Pegando o primeiro byte da string
+    addi a1, a1, -48       # Transferindo para numero
     addi a2, a2, 1
-    addi a3, a3, a1
+    add a3, a3, a1
    
 
-    lb a1, count(a0)        # Pegando o primeiro byte da string
-    addi a1, a1, -'0'       # Transferindo para numero
+    lb a1, 2(a0)        # Pegando o primeiro byte da string
+    addi a1, a1, -48      # Transferindo para numero
     addi a2, a2, 1
     mul a1, a1, a7
-    addi a3, a3, a1
+    add a3, a3, a1
     
-    lb a1, count(a0)        # Pegando o primeiro byte da string
-    addi a1, a1, -'0'       # Transferindo para numero
-    addi a2, a2, 1
+    lb a1, 1(a0)        # Pegando o primeiro byte da string
+    addi a1, a1, -48      # Transferindo para numero
     mul a1, a1, a7
     mul a1, a1, a7
-    addi a3, a3, a1
+    add a3, a3, a1
     
-    lb a1, count(a0)        # Pegando o primeiro byte da string
-    addi a1, a1, -'0'       # Transferindo para numero
-    addi a2, a2, 1
+    lb a1, 0(a0)        # Pegando o primeiro byte da string
+    addi a1, a1, -48       # Transferindo para numero
     mul a1, a1, a7
     mul a1, a1, a7
     mul a1, a1, a7
-    addi a3, a3, a1
+    add a3, a3, a1
 
-    la a5, num
-    sw a2, 0(a5)
+    ret
+
+Sqrt:
+    li a7,2
+    li a6,0
+    li a5,10
+    div a2,a3,a7
+    1:
+        div a4,a3,a2
+        add a2,a2,a4
+        div a2,a2,a7
+        addi a6,a6,1
+        blt a6,a5,1b
     ret
 
 Int_Str:
+    li a7,10
     
-    
+
+    rem a4,a2,a7
+    div a2,a2,a7
+    addi a4,a4,'0'
+    sb a4, 3(s0)
+
+    rem a4,a2,a7
+    div a2,a2,a7
+    addi a4,a4,'0'
+    sb a4, 2(s0)
+
+    rem a4,a2,a7
+    div a2,a2,a7
+    addi a4,a4,'0'
+    sb a4, 1(s0)
+
+    rem a4,a2,a7
+    div a2,a2,a7
+    addi a4,a4,'0'
+    sb a4, 0(s0)
+
+    ret
+
+
 
 .globl _start
 _start:
+    
     jal ra, read
+    la a0, input
+    
+    la s0, output
+    #Transformar uma string em int tirar sua raiz e inserir o int em uma string 
 
+    # Number 1
     jal ra, Str_Int
-
-
-    jal ra, write
+    jal ra, Sqrt
+    jal ra, Int_Str
+    li a5, ' '
+    sb a5, 4(s0)
     
-    
+    # Number 2
+    addi a0, a0, 5
+    addi s0, s0, 5
+    jal ra, Str_Int
+    jal ra, Sqrt
+    jal ra, Int_Str
+    li a5, ' '
+    sb a5, 4(s0)
+
+    # Number 3
+    addi a0, a0, 5
+    addi s0, s0, 5
+    jal ra, Str_Int
+    jal ra, Sqrt
+    jal ra, Int_Str
+    li a5, ' '
+    sb a5, 4(s0)
+
+    # Number 4
+    addi a0, a0, 5
+    addi s0, s0, 5
+    jal ra, Str_Int
+    jal ra, Sqrt
+    jal ra, Int_Str
+    li a5, '\n'
+    sb a5, 4(s0)
+
+    jal ra,write
